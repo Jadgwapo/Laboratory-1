9c22063e4681b49d91fb1869b607e5b268455133
Jad Lucero <160345454+Jadgwapo@users.noreply.github.com>
2025-08-07 18:02:41 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -38,3 +38,40 @@ int main () {
             return 0;
 
 }
+void showMenu() {
+    cout << "=== Directory Management System ===\n";
+    cout << "[1] List Files\n";
+    cout << "[2] Create Directory\n";
+    cout << "[3] Change Directory\n";
+    cout << "[4] Exit\n";
+}
+void listFiles() {
+    cout << "\nFiles in current directory: \n";
+    system("dir");
+}
+void createDirectory() {
+    string folderName;
+    cout << "Enter directory name: ";
+    cin >> folderName;
+
+    int result = _mkdir(folderNmae.c_str());
+    if (result == 0){
+        cout << "Directory \"" << folderName << "\" created successfully. \n";
+    } else {
+        cout << "Error: Directory might already exist. \n";
+    }   
+}
+void changeDirectory() {
+    string path;
+    cout << "Enter path to change directory: ";
+    cin.ignore();
+    getline(cin, path);
+
+    if(SetCurrentDirectory(path.c_str())) {
+        char currentPath[MAX_PATH];
+        GetCurrentDirectory(MAX_PATH, currentPath);
+        cout << "Change directory to: " << currrentPath << endl;
+    } else {
+        cout << "Error: Path not found\n";
+    }
+}
